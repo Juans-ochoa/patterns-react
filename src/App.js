c9168208e0935c8307ad2ead0f688b3ebe8e62f4
Juans Ochoa <sebastian.ochoa.dev@gmail.com>
2022-05-12 20:38:31 -0500

    Base enviroment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { SplitScreen } from "./SplitScreen";
-
-const LeftHandComponent = () => {
-  return <h1 style={{ backgroundColor: "red" }}>Left!</h1>;
-};
-const RightHandComponent = () => {
-  return <p style={{ backgroundColor: "blueviolet" }}>Right!</p>;
-};
-
 function App() {
   return (
     <>
-      <SplitScreen left={LeftHandComponent} right={RightHandComponent} />
+      <p>your code here</p>
     </>
   );
 }
