8e6b92dbd83577662db7e1c09a147623b48ab4ee
Sebastian Ochoa <sebastian.ochoa.dev@gmail.com>
2022-05-19 23:44:10 -0500

    Class 02_06b

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import axios from "axios";
+import { DataSource } from "./DataSource";
 import { ProductInfo } from "./ProductInfo";
 import { ResourceLoader } from "./ResourceLoader";
 import { UserInfo } from "./UserInfo";
 
+const getServerData = (url) => async () => {
+  const res = await axios.get(url);
+  return res.data;
+};
+
 function App() {
   return (
     <>
-      <ResourceLoader resourceUrl="/users/123" resourceName="user">
+      <DataSource getDataFunc={getServerData("/users/123")} resourceName="user">
         <UserInfo />
-      </ResourceLoader>
-      <ResourceLoader resourceUrl="/products/1234" resourceName="product">
-        <ProductInfo />
-      </ResourceLoader>
+      </DataSource>
     </>
   );
 }
