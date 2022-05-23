183087684fcf573545570da3021f8061be09f8ed
Sebastian Ochoa <sebastian.ochoa.dev@gmail.com>
2022-05-22 21:48:18 -0500

    Class 03_05b

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import { ControlledForm } from "./ControlledForm";
-import { UncontrolledForm } from "./UncontrolledForm";
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+
+const StepOne = ({ goToNext }) => (
+  <>
+    <h1>Step 1</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+
+const StepTwo = ({ goToNext }) => (
+  <>
+    <h1>Step 2</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+
+const StepThree = ({ goToNext }) => (
+  <>
+    <h1>Step 3</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
 
 function App() {
   return (
-    <>
-      <ControlledForm />
-    </>
+    <UncontrolledOnboardingFlow>
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledOnboardingFlow>
   );
 }
 
