d79ed6fa9f4e096d59ddfe567081674f2a300387
Sebastian Ochoa <sebastian.ochoa.dev@gmail.com>
2022-05-29 00:07:58 -0500

    04_05b

---
+++ src/withEditableResource.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+
+export const withEditableResource = (Component, resourcePath, resourceName) => {
+  return (props) => {
+    const [originalData, setOriginalData] = useState(null);
+    const [data, setData] = useState(null);
+
+    const onChange = (changes) => {
+      setData({ ...data, ...changes });
+    };
+
+    const onSave = async () => {
+      const response = await axios.post(resourcePath, { [resourceName]: data });
+      setOriginalData(response.data);
+      setData(response.data);
+    };
+
+    const onReset = () => setData(originalData);
+
+    useEffect(() => {
+      (async () => {
+        const response = await axios.get(resourcePath);
+        setOriginalData(response.data);
+        setData(response.data);
+      })();
+    }, []);
+
+    const resourceProps = {
+      [resourceName]: data,
+      [`onChange${capitalize(resourceName)}`]: onChange,
+      [`onSave${capitalize(resourceName)}`]: onSave,
+      [`onReset${capitalize(resourceName)}`]: onReset,
+    };
+
+    return <Component {...props} {...resourceProps} />;
+  };
+};
